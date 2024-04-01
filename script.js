0816d8b413776d7db74cf478ac7246990647fb23
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-01 15:17:59 -0300

    fix so much bugs

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
-Number.prototype.toLocaleString('pt-br')
+Number.prototype.toLocaleString("pt-br");
 
-const display = document.querySelector("#idexpression")
+const display = document.querySelector("#idexpression");
 
-const displayInArea = value => {
-    display.value += value;
+const checkLength = (value) => {
+  if (value.length > 13) {
+    display.classList.add("moreChar");
+  } else {
+    display.classList.remove("moreChar");
+  }
+};
+
+const displayInArea = (value) => {
+  display.setAttribute("placeholder", "000");
+
+  display.value += value;
+
+  let especialChar = ["*", "-", "+", "/", ","];
+
+  especialChar.map((item) => {
+    if (display.value === item) {
+      display.value = "";
+    }
+  });
+  checkLength(display.value);
 };
 
 const backspace = () => {
-    let value = display.value;
-    value = value.split('');
-    value.pop();
-    value = value.join('');
+  let value = display.value;
+  value = value.split("");
+  value.pop();
+  value = value.join("");
 
-    display.value = value;
-    return;
-}
+  display.value = value;
+
+  checkLength(display.value);
+  return;
+};
 
 const clean = () => {
-    display.value ='';
-    return;
-}
+  display.value = "";
+
+  checkLength(display.value);
+  return;
+};
 
 const calculate = () => {
-    let value = display.value;
-    display.value = eval(value);
-}
+  let value = display.value;
+
+  let val = eval(value);
+
+  if (value && val !== "Infinity") {
+    if (val.length > 21) {
+      display.value = val.toExponential();
+
+      checkLength(display.value);
+    } else {
+      display.value = val;
+
+      checkLength(display.value);
+    }
+  } else if (val === "Infinity") {
+    display.setAttribute("placeholder", "Infinity");
+
+    checkLength(display.value);
+  } else {
+    display.setAttribute("placeholder", "Error");
+
+    checkLength(display.value);
+  }
+};
